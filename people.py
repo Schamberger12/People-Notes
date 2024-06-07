e7220ee5277477be8eb54ff85e2650920fd9dac8
Schamberger12 <Schamberger12@gmail.com>
2024-06-07 10:24:03 -0500

    swagger & methods

--- people.py
+++ people.py
@@ -1,6 +1,7 @@
 # people.py
 
 from datetime import datetime
+from flask import abort, make_response
 
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
@@ -25,3 +26,47 @@ PEOPLE = {
 
 def read_all():
     return list(PEOPLE.values())
+
+def create(person):
+    lname = person.get("lname")
+    fname = person.get("fname")
+
+    if lname and fname not in PEOPLE:
+        PEOPLE[lname], 201
+    else:
+        abort(
+            406, 
+            f"person with last name {lname} alread exists"
+        )
+
+def read_one(lname):
+    if lname in PEOPLE:
+        return PEOPLE[lname]
+    else:
+        abort(
+            404, 
+            f"Person with last name{lname} not found"
+        )
+
+def update(lname, person):
+    if lname in PEOPLE:
+        PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
+        PEOPLE[lname]["timestamp"] = get_timestamp()
+        return PEOPLE[lname]
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
+        )
+
+def delete(lname):
+    if lname in PEOPLE:
+        del PEOPLE[lname]
+        return make_response(
+            f"{lname} successfully deleted", 200
+        )
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
+        )
